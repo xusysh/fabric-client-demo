24b9003c8fe5f1245dc40bdb1ff4b0c445d72151
gjy gjy <xusysh@126.com>
2020-11-16 20:29:06 +0800

    1116

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,32 @@
     <v-main>
       <HelloWorld />
     </v-main>
+
+    <v-bottom-navigation v-model="tab" :background-color="color" dark shift>
+      <v-btn>
+        <span>Video</span>
+
+        <v-icon>mdi-television-play</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Music</span>
+
+        <v-icon>mdi-music-note</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Book</span>
+
+        <v-icon>mdi-book</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Image</span>
+
+        <v-icon>mdi-image</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
@@ -48,8 +74,24 @@ export default {
   components: {
     HelloWorld
   },
-
+  computed: {
+    color() {
+      switch (this.tab) {
+        case 0:
+          return "blue-grey";
+        case 1:
+          return "teal";
+        case 2:
+          return "brown";
+        case 3:
+          return "indigo";
+        default:
+          return "blue-grey";
+      }
+    }
+  },
   data: () => ({
+    tab: ""
     //
   }),
   created() {
