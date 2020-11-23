8b7157f69567b9469884d90551b951faaa4fb494
gjy gjy <xusysh@126.com>
2020-11-23 11:47:13 +0800

    浮动按钮

---
+++ src/views/components/fab/AddItem/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-fab-transition>
+        <v-speed-dial
+          v-model="fab"
+          fixed
+          style="bottom: 80px;left:40px"
+          :top="top"
+          :bottom="bottom"
+          :right="right"
+          :left="left"
+          :direction="direction"
+          :open-on-hover="hover"
+          :transition="transition"
+        >
+          <template v-slot:activator>
+            <v-btn
+              v-model="fab"
+              color="blue darken-2"
+              dark
+              fab
+              x-small
+              v-on="on"
+            >
+              <v-icon v-if="fab">mdi-close</v-icon>
+              <v-icon v-else>mdi-plus</v-icon>
+            </v-btn>
+          </template>
+          <v-btn fab dark small color="red" disabled>
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                fab
+                dark
+                small
+                color="green"
+                v-on="on"
+                @click="OpenDialog()"
+              >
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+            </template>
+            <span>新建文章</span>
+          </v-tooltip>
+        </v-speed-dial>
+      </v-fab-transition>
+    </template>
+    <span>操作</span>
+  </v-tooltip>
+</template>
+
+<script>
+// Utilities
+export default {
+  data: () => ({
+    direction: "top",
+    fab: false,
+    fling: false,
+    hover: false,
+    tabs: null,
+    top: false,
+    right: false,
+    bottom: true,
+    left: true,
+    transition: "slide-y-reverse-transition"
+  }),
+  methods: {
+    OpenDialog() {
+      this.$parent.$parent.OpenArticleDialog()
+    }
+  }
+}
+</script>
+
+<style scoped>
+#create .v-speed-dial {
+  position: absolute;
+  bottom: 160px;
+  right: 40px;
+}
+
+#create .v-btn--floating {
+  position: relative;
+}
+</style>
---
+++ src/views/components/fab/ReturnToTop/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-fab-transition>
+        <v-btn
+          v-on="on"
+          v-show="fab"
+          v-scroll="onScroll"
+          style="bottom: 80px;right:40px"
+          aria-label="Scroll to top"
+          color="red"
+          x-small
+          dark
+          fab
+          fixed
+          bottom
+          right
+          title="Scroll to top"
+          @click="toTop"
+        >
+          <v-icon>mdi-chevron-up</v-icon>
+        </v-btn>
+      </v-fab-transition>
+    </template>
+    <span>返回顶部</span>
+  </v-tooltip>
+</template>
+
+<script>
+// Utilities
+export default {
+  data: () => ({ fab: false }),
+  methods: {
+    onScroll() {
+      if (typeof window === "undefined") return
+      const top = window.pageYOffset || document.documentElement.offsetTop || 0
+      this.fab = top > 80
+    },
+    toTop() {
+      //   this.$router.push({ hash: '' })
+      this.$vuetify.goTo(0)
+    }
+  }
+}
+</script>
