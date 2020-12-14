47becd6681387d2dfb4d97ab591972c050155fd6
gjy gjy <xusysh@126.com>
2020-12-14 21:03:22 +0800

    账务信息样式

--- src/views/pages/FinancialInfo/index.vue
+++ src/views/pages/FinancialInfo/index.vue
@@ -14,10 +14,10 @@
         工会
       </v-tab>
       <v-tab-item v-for="n in 2" :key="n" disabled>
-        <v-card style="margin: 5vh 0">
+        <v-card style="padding: 20px 0;background: #333333">
           <bar-animation-delay></bar-animation-delay>
         </v-card>
-        <v-card>
+        <v-card style="padding-bottom:20px;background: #333333">
           <nested-pies></nested-pies>
         </v-card>
       </v-tab-item>
