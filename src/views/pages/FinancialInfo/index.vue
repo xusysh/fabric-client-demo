0a38459418ed6b24c085d3b5f8a375c3d741f377
xusysh <xusysh@126.com>
2020-12-18 23:39:57 +0800

    1218

--- src/views/pages/FinancialInfo/index.vue
+++ src/views/pages/FinancialInfo/index.vue
@@ -80,41 +80,73 @@ export default {
   methods: {
     async getFinLocInfo() {
       this.finLocInfoLoading = true
-      const { data } = await this.$axios.get(
-        `/stat/fin-loc-info/${
-          this.curTab === "union" ? "gonghui.js" : this.curUserInfo.userId
-        }`
-      )
-      console.log(data)
-      this.finLocLegendList = data.data.map(
-        item => this.idToName[item.targetId]
-      )
-      this.finLocDataList = data.data.map(item => {
-        return { value: item.amount, name: this.idToName[item.targetId] }
-      })
-      console.log(this.finLocLegendList)
-      console.log(this.finLocDataList)
-      this.$refs.finLocChart[0].init()
-      this.finLocInfoLoading = false
+      try {
+        const { data } = await this.$axios.get(
+          `/stat/fin-loc-info/${
+            this.curTab === "union" ? "gonghui.js" : this.curUserInfo.userId
+          }`
+        )
+        console.log(data)
+        this.finLocLegendList = data.data.map(
+          item => this.idToName[item.targetId]
+        )
+        this.finLocDataList = data.data.map(item => {
+          return { value: item.amount, name: this.idToName[item.targetId] }
+        })
+        console.log(this.finLocLegendList)
+        console.log(this.finLocDataList)
+        this.$refs.finLocChart[0].init()
+        this.finLocInfoLoading = false
+      } catch (ex) {
+        this.$toast.success("生成资金流向信息失败", {
+          position: "top-right",
+          timeout: 1500,
+          closeOnClick: true,
+          draggable: true,
+          draggablePercent: 0.4,
+          showCloseButtonOnHover: false,
+          hideProgressBar: false,
+          closeButton: "button",
+          icon: true,
+          rtl: false
+        })
+        this.finLocInfoLoading = false
+      }
     },
     async getFinInfo() {
       this.finInfoLoading = true
-      const { data } = await this.$axios.post(`/stat/fin-info`, {
-        userId:
-          this.curTab === "union" ? "gonghui.js" : this.curUserInfo.userId,
-        startTime: "2020-12-01 00:00:00",
-        endTime: "2021-12-01 00:00:00"
-      })
-      this.finDates = []
-      this.finDataListSeries = [[], [], []]
-      for (const finInfo of data.data) {
-        this.finDates.push(finInfo.date)
-        this.finDataListSeries[0].push(finInfo.income || 0)
-        this.finDataListSeries[1].push(finInfo.expense || 0)
-        this.finDataListSeries[2].push(finInfo.totalBalance || 0)
+      try {
+        const { data } = await this.$axios.post(`/stat/fin-info`, {
+          userId:
+            this.curTab === "union" ? "gonghui.js" : this.curUserInfo.userId,
+          startTime: "2020-12-01 00:00:00",
+          endTime: "2021-12-01 00:00:00"
+        })
+        this.finDates = []
+        this.finDataListSeries = [[], [], []]
+        for (const finInfo of data.data) {
+          this.finDates.push(finInfo.date)
+          this.finDataListSeries[0].push(finInfo.income || 0)
+          this.finDataListSeries[1].push(finInfo.expense || 0)
+          this.finDataListSeries[2].push(finInfo.totalBalance || 0)
+        }
+        console.log(data)
+        this.finInfoLoading = false
+      } catch (ex) {
+        this.$toast.success("生成收支情况信息失败", {
+          position: "top-right",
+          timeout: 1500,
+          closeOnClick: true,
+          draggable: true,
+          draggablePercent: 0.4,
+          showCloseButtonOnHover: false,
+          hideProgressBar: false,
+          closeButton: "button",
+          icon: true,
+          rtl: false
+        })
+        this.finLocInfoLoading = false
       }
-      console.log(data)
-      this.finInfoLoading = false
     },
     tabChange(tab) {
       this.curTab = tab === 1 ? "union" : "user"
